be9c0ba5ed72cad1ecb1c98b304c30fec82f66ed
santos <santos.sanchez@bue.edu.ar>
2023-01-09 21:28:55 -0300

    routing en angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienciaComponent } from '../app/components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'experiencia', component:ExperienciaComponent},
+  {path: 'educacion', component:EducacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
