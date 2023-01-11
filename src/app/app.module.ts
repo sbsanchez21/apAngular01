987552ee83600bcb43d008e19849ebf580a04f9e
santos <santos.sanchez@bue.edu.ar>
2023-01-10 21:26:25 -0300

    directivas, databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,14 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     HeaderComponent,
     FooterComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    EncabezadoComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-acerca-de',
+  templateUrl: './acerca-de.component.html',
+  styleUrls: ['./acerca-de.component.css']
+})
+export class AcercaDeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  acercaDe:string ='Matemático, Fisico e Ingenierio del INTI';
+
+
+}
